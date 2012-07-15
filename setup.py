385b7762fbd5ec4829552f65b91e865ef3fa4da6
Josh Gachnang <Josh@ServerCobra.com>
2012-07-15 00:14:50 -0500

    Prepared setup for push to PyPi

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name='mag',
+    name='MAGPlus',
     version='0.0.9',
     author='Josh Gachnang',
     author_email='Josh@ServerCobra.com',
-    packages=['mag',],
+    packages=['magplus',],
     #scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
-    url='http://pypi.python.org/pypi/mag/',
+    url='http://pypi.python.org/pypi/MAGPlus/',
     license='LICENSE.txt',
     description='Retrieving latest Minecraft versions.',
     long_description=open('README.rst').read(),
